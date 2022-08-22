c125f766a331d604c2b1e33ac3581a76a034b4bc
Brian Keegan <brian.morris.keegan@gmail.com>
2022-08-22 09:35:27 -0400

    Quick refactor

--- content.js
+++ content.js
@@ -3,43 +3,10 @@ console.log('content.js is started');
 
 
 // Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
+const config = { childList: true, subtree: false };
 
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
+let hasStartedObserving = false;
 
-    const skipButton = document.querySelector('.ytp-ad-skip-button');
-    const closeAdButtom = document.querySelector('.ytp-ad-overlay-close-button');
-    if (skipButton) {
-        console.log('skipButton is found');
-        skipButton.click();
-    }
-    if (closeAdButtom) {
-        console.log('closeAdButtom is found');
-        closeAdButtom.click();
-    }
-    // const className = mutation?.target?.className
-    // if (
-    //     className?.includes('ytp-ad-skip-button') 
-    //     || className?.includes('ytp-ad-overlay-close-button'
-    // )) {
-    //     console.log('skipped add!');
-    //     mutation.target.click();
-    // }
-    // // 
-    // if (mutation.type === 'childList') {
-    //     console.log(mutation);
-    //   console.log('A child node has been added or removed.');
-    // } else if (mutation.type === 'attributes') {
-    //      console.log(mutation);
-    //   console.log(`The ${mutation.attributeName} attribute was modified.`);
-    // }
-  }
-};
-
-
-// ====,f
 
 
 
@@ -48,13 +15,8 @@ const ALLOWED_TO_OPEN = ['/'];
 
 const POSSIBLE_OBJECTS_2 = ['isAllowedUrl', 'isOn', 'token', 'url', 'tabId', 'tabUrl']
 
-const observer = new MutationObserver(callback);
-const skipAds = async () => {
-    const { tabUrl } = await chrome.storage.local.get(POSSIBLE_OBJECTS_2);
-    console.log(tabUrl.includes('youtube.com'))
-    // youtube case:
-    if (tabUrl.includes('youtube.com')) {
-        // const adsContainer = document.querySelector('.ytp-ad-message-container');
+const observeYouTube = async () => {
+            // const adsContainer = document.querySelector('.ytp-ad-message-container');
         const adsContainer = document.querySelector('.video-ads');
         const skipButton = document.querySelector('.ytp-ad-skip-button');
         const closeAdButtom = document.querySelector('.ytp-ad-overlay-close-button');
@@ -68,12 +30,36 @@ const skipAds = async () => {
         }
         if (adsContainer) {
             console.log('observer hapenning');
-            observer.observe(adsContainer, config);
+            observer = new MutationObserver((mutationList, observer) => {
+                for (let i = 0; i < mutationList.length; i++) {
+
+                    const skipButton = document.querySelector('.ytp-ad-skip-button');
+                    const closeAdButtom = document.querySelector('.ytp-ad-overlay-close-button');
+                    if (skipButton) {
+                        console.log('skipButton is found');
+                        skipButton.click();
+                    }
+                    if (closeAdButtom) {
+                        console.log('closeAdButtom is found');
+                        closeAdButtom.click();
+                    }
+                }
+            })
+            .observe(adsContainer, config)
         } else {
             setTimeout(() => {
-                skipAds();
+                observeYouTube();
             }, 100);
         }
+    }
+
+let observer = null;
+const skipAds = async () => {
+    const { tabUrl } = await chrome.storage.local.get(POSSIBLE_OBJECTS_2);
+    console.log(tabUrl.includes('youtube.com'))
+    // youtube case:
+    if (tabUrl.includes('youtube.com')) {
+        observeYouTube();
     }
     
 }
@@ -82,22 +68,23 @@ const startOrStop = async () => {
     const { isAllowedUrl, isOn } = await chrome.storage.local.get(POSSIBLE_OBJECTS_2);
     console.log({isAllowedUrl, isOn})
     if (isAllowedUrl && isOn) {
-        observer = new MutationObserver(callback);
         skipAds();
     } 
     if (!isOn) {
-        observer.disconnect();
+        if (Boolean(observer)) {
+            observer.disconnect();
+        }
     }
 }
 
 
 
 const validateUrlIsAllowed = async (tabUrl) => {
+    const isAllowedUrl = ALLOWED_TO_OPEN.some(url => tabUrl.includes(url));
     if (!isAllowedUrl) {
         console.log('content: url is not allowed, ', tabUrl);
         return false;
     }
-    const isAllowedUrl = ALLOWED_TO_OPEN.some(url => tabUrl.includes(url));
     chrome.storage.local.set({ isAllowedUrl, tabUrl });
 }
 
