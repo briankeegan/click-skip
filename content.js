1e814a62deca59d3ffc8d8972daaeeb7a66f538c
Brian Keegan <brian.morris.keegan@gmail.com>
2022-08-23 21:11:20 -0400

    Sign something

--- content.js
+++ content.js
@@ -15,58 +15,66 @@ const ALLOWED_TO_OPEN = ['/'];
 
 const POSSIBLE_OBJECTS_2 = ['isAllowedUrl', 'isOn', 'token', 'url', 'tabId', 'tabUrl']
 
-const observeYouTube = async () => {
-            // const adsContainer = document.querySelector('.ytp-ad-message-container');
-        const adsContainer = document.querySelector('.video-ads');
-        const skipButton = document.querySelector('.ytp-ad-skip-button');
-        const closeAdButtom = document.querySelector('.ytp-ad-overlay-close-button');
-        if (skipButton) {
-            console.log('skipButton is found');
-            skipButton.click();
-        }
-        if (closeAdButtom) {
-            console.log('closeAdButtom is found');
-            closeAdButtom.click();
-        }
-        if (adsContainer) {
-            console.log('observer hapenning');
-            observer = new MutationObserver((mutationList, observer) => {
-                for (let i = 0; i < mutationList.length; i++) {
 
-                    const skipButton = document.querySelector('.ytp-ad-skip-button');
-                    const closeAdButtom = document.querySelector('.ytp-ad-overlay-close-button');
-                    if (skipButton) {
-                        console.log('skipButton is found');
-                        skipButton.click();
-                    }
-                    if (closeAdButtom) {
-                        console.log('closeAdButtom is found');
-                        closeAdButtom.click();
-                    }
-                }
-            })
-            .observe(adsContainer, config)
-        } else {
-            setTimeout(() => {
-                observeYouTube();
-            }, 100);
-        }
+
+// Make this into reusable
+// take array of possible things to skip
+// array of container(s) to observe.
+
+const youTubeContainerSelector = '.video-ads';
+const youTubeClickableSelectors = ['.ytp-ad-skip-button', '.ytp-ad-overlay-close-button'];
+
+const findAndClick = async (selector) => {
+    const element = await document.querySelector(selector);
+    if (element) {
+        console.log('found element and clicked', selector);
+        element.click();
     }
+}
+
+const observeContainerAndButtons = async ({
+    containerSelector,
+    clickableSelectors,
+}) => {
+    const adsContainer = document.querySelector(containerSelector);
+    // await clickableSelectors.forEach(findAndClick);
+    clickableSelectors.forEach(findAndClick);
+    
+    if (adsContainer) {
+        console.log('observer hapenning');
+        observer = new MutationObserver((mutationList, observer) => {
+            for (let i = 0; i < mutationList.length; i++) {
+                clickableSelectors.forEach(findAndClick);
+            }
+        })
+        .observe(adsContainer, config)
+    } else {
+        setTimeout(() => {
+            observeContainerAndButtons({
+                containerSelector,
+                clickableSelectors,
+            });
+        }, 100);
+    }
+}
 
 let observer = null;
 const skipAds = async () => {
     const { tabUrl } = await chrome.storage.local.get(POSSIBLE_OBJECTS_2);
-    console.log(tabUrl.includes('youtube.com'))
     // youtube case:
     if (tabUrl.includes('youtube.com')) {
-        observeYouTube();
+        observeContainerAndButtons({
+            containerSelector: youTubeContainerSelector,
+            clickableSelectors: youTubeClickableSelectors,
+        });
     }
     
 }
+
+
 // https://www.youtube.com/watch?v=gHnuQZFxHt0
 const startOrStop = async () => {
     const { isAllowedUrl, isOn } = await chrome.storage.local.get(POSSIBLE_OBJECTS_2);
-    console.log({isAllowedUrl, isOn})
     if (isAllowedUrl && isOn) {
         skipAds();
     } 
